45590640131720c425b4d21a7c7c2700a329f66d
SChoumiloff <sacha@la-forge.ai>
2023-11-05 21:00:34 +0100

    mise a jour du logo et suppression des css

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,32 @@
-import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
-import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
-import { ToastContainer } from "react-toastify";
-
-
+import Head from "next/head";
 
 export default function Home() {
   
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div className="container mt-24 mx-auto px-12 py-4">
-        <Navbar />
-        <HeroSection />
-        <AchievementsSection />
-        <AboutSection />
-        <ProjectsSection />
-        <EmailSection />
-      </div>
-    </main>
+    <html>
+      <head>
+        <link rel="icon" href="/images/logo.png" />
+        <title>Sacha Choumiloff</title>
+      </head>
+      <body>
+        <main className="flex min-h-screen flex-col bg-[#121212]">
+          <div className="container mt-24 mx-auto px-12 py-4">
+            <Navbar />
+            <HeroSection />
+            <AchievementsSection />
+            <AboutSection />
+            <ProjectsSection />
+            <EmailSection />
+          </div>
+        </main>    
+      </body>
+    </html>
+    
   );
 }
