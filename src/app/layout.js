39582c5e5f1b658a0678ae9a2a462961e9009b83
SChoumiloff <sacha@la-forge.ai>
2023-11-07 07:34:04 +0100

    layout for google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,12 +14,23 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/images/logo.png" />
         <title>{metadata.title}</title>
         <meta name='description' content={metadata.description}/>
-        <meta name="keywords" content="data engineering, portfolio, mountains, Sacha Choumiloff" />
+        <meta name="keywords" content="data engineering, data elevation, portfolio, mountains, Sacha Choumiloff" />
         <meta name="robots" content="index, follow" />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta http-equiv="content-language" content="en" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-34PTC9DSKE"></script>
+        <script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-34PTC9DSKE');
+        `}
+        </script>
       </head>
+      
       <body className={inter.className}>{children}</body>
     </html>
   )
