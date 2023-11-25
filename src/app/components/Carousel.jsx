1f7b04225f0cf44c3aa4160789a6827db31c55fb
SChoumiloff <sacha@la-forge.ai>
2023-11-25 15:48:05 +0100

    ajout d'un effet sur l'image pour l'agrandir :)

--- src/app/components/Carousel.jsx
+++ src/app/components/Carousel.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Image from 'next/image';
 
 const Carousel = () => {
+  
+  const [enlargedImage, setEnlargedImage] = useState(null);
+
   const settings = {
     dots: true,
     infinite: true,
@@ -13,31 +16,42 @@ const Carousel = () => {
     slidesToScroll: 1,
   };
 
+  const enlargeImage = (imageSrc) => {
+    setEnlargedImage(imageSrc);
+  };
+
+  const closeEnlarged = () => {
+    setEnlargedImage(null);
+  };
+
   return (
       <div className="carousel-container">
       <Slider {...settings}>
-        {/* <div className="carousel-item">
-          <img src="/images/about/img1.JPEG" alt="Image 1" width={500} height={500} />
-        </div> */}
-        <div className="carousel-item">
+        <div className="carousel-item" onClick={() => enlargeImage("/images/about/SChoumiloffEVE.jpeg")}>
           <img src="/images/about/SChoumiloffEVE.jpeg" alt="Everest (Nepal)"  />
         </div>
-        <div className="carousel-item">
+        <div className="carousel-item" onClick={() => enlargeImage("/images/about/SChoumiloffLC.jpeg")}>
           <img src="/images/about/SChoumiloffLC.jpeg" alt="Saint véran (Queyras)"  />
         </div>
-        <div className="carousel-item">
+        <div className="carousel-item" onClick={() => enlargeImage("/images/about/SChoumiloffLC2.jpeg")}>
           <img src="/images/about/SChoumiloffLC2.jpeg" alt="Saint véran 2(Queyras)"  />
         </div>
-        <div className="carousel-item">
+        <div className="carousel-item" onClick={() => enlargeImage("/images/about/SChoumiloffMB.jpeg")}>
           <img src="/images/about/SChoumiloffMB.JPEG" alt="Mont blanc (Tournette)" />
         </div>
-        <div className="carousel-item">
+        <div className="carousel-item" onClick={() => enlargeImage("/images/about/SChoumiloffNEP.jpeg")}>
           <img src="/images/about/SChoumiloffNEP.jpeg" alt="(Nepal)" />
         </div>
-        <div className="carousel-item">
+        <div className="carousel-item" onClick={() => enlargeImage("/images/about/SChoumiloffTAIL.jpeg")}>
           <img src="/images/about/SChoumiloffTAIL.jpeg" alt="Taillante (Queyras)" />
         </div>
       </Slider>
+      {enlargedImage && (
+        <div className="enlarged-image-overlay" onClick={closeEnlarged}>
+          <img src={enlargedImage} alt="Enlarged" className="enlarged-image" />
+        </div>
+      )}
+
     </div>
   );
 };
